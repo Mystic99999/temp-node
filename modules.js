cf153725ecfa6a6aa852a8b5b1c37a1e2c904eb1
Mystic999 <eyesd676@gmail.com>
2024-04-19 19:14:14 +0545

    first commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative')
+console.log(data)
+
+require('./mind-grenade')
+
+sayHi('Ram')
+sayHi(names.john)
+sayHi(names.peter)
